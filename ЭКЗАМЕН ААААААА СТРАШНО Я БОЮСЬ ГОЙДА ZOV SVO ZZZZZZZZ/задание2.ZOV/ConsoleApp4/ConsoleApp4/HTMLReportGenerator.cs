b32c4e7236819a917bea88630b622ae8522f4405
hakkumi <hakkumi1337@gmail.com>
2025-11-27 11:05:34 +0300

    Add files via upload

---
+++ ЭКЗАМЕН ААААААА СТРАШНО Я БОЮСЬ ГОЙДА ZOV SVO ZZZZZZZZ/задание2.ZOV/ConsoleApp4/ConsoleApp4/HTMLReportGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class HTMLReportGenerator : IReportGenerator
+    {
+        public string Generate<T>(List<T> data)
+        {
+            string result = "<html><body><h2>HTML Report</h2><ul>";
+
+            foreach (var item in data)
+            {
+                result += "<li>" + item.ToString() + "</li>";
+            }
+
+            result += "</ul></body></html>";
+            return result;
+        }
+    }
+
+
+}
