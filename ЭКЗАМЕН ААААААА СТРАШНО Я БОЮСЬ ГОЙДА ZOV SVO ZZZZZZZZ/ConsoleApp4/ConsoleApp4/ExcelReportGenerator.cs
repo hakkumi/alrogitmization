b4707f83fde04a46a0b8ffe5740e9b3463733851
hakkumi <hakkumi1337@gmail.com>
2025-11-27 11:02:40 +0300

    Add files via upload

---
+++ ЭКЗАМЕН ААААААА СТРАШНО Я БОЮСЬ ГОЙДА ZOV SVO ZZZZZZZZ/ConsoleApp4/ConsoleApp4/ExcelReportGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+
+    public class ExcelReportGenerator : IReportGenerator
+    {
+        public string Generate<T>(List<T> data)
+        {
+            string result = "=== EXCEL REPORT ===\n";
+
+            foreach (var item in data)
+            {
+                result += item.ToString() + "\t|\n";
+            }
+
+            return result;
+        }
+    }
+
+
+}
